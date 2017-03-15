484ee82fe625c02aa0d671dcd90ca4f60701890b
wiramahardika <putudikka@gmail.com>
2017-03-16 00:03:21 +0700

    update

--- vagrant-excercise-1/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-apt-get update
-apt-get install -y apache2
-sudo apt-get install -y python-software-properties
-sudo apt-add-repository ppa:ondrej/php
-sudo apt-get update
-sudo apt-get install -y php7.0
